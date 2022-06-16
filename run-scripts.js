f57da394d39501927561bea56f4f0b1928928e05
tyschreiver <tyschreiver@gmail.com>
2022-06-16 00:43:37 -0400

    starts run scripts

--- run-scripts.js
+++ run-scripts.js
@@ -1,2 +1,12 @@
 const ngrok = require("ngrok")
 const fs = require("fs")
+const spawn = require("child_process").spawn
+require("dotenv").config()
+
+const startServerScript = "node server.js"
+
+const startServer = spawn(startServerScript, { cwd: process.env.DIRECTORY, shell: true })
+startServer.stdout.setEncoding("utf8")
+startServer.stdout.on("data", data => {
+  console.log("data")
+})
